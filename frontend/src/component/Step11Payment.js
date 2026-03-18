b1b09c0c017d2cc2735ebb16e926aafeedd114d1
Rooban sankar <rooban1752000@gmail.com>
2026-03-18 14:38:43 +0530

    added payment

--- frontend/src/component/Step11Payment.js
+++ frontend/src/component/Step11Payment.js
@@ -7,6 +7,7 @@ export default function Step11Payment({ formData, setFormData }) {
   const [plan, setPlan] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const [userType, setUserType] = useState("");
   
 
 
@@ -155,6 +156,106 @@ useEffect(() => {
   //     </button>
   //   </div>
   // );
+// return (
+//   <div className="max-w-4xl mx-auto mt-10 bg-white p-8 rounded-xl shadow text-center">
+//     <h2 className="text-xl font-bold mb-8">Choose Your Plan</h2>
+
+//     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+//       {/* OLD USER */}
+//       <div className="border rounded-lg p-6">
+//         <h3 className="text-lg font-semibold mb-2">Old Users</h3>
+//         <p className="text-sm text-gray-500 mb-6">
+//           Already joined in WhatsApp group
+//         </p>
+
+//         <div
+//           onClick={() => {
+//             setPlan("basic");
+//             setUserType("old");
+//           }}
+//           className={`p-6 border rounded-lg cursor-pointer ${
+//             plan === "basic" && userType === "old"
+//               ? "border-rose-500 bg-rose-50"
+//               : ""
+//           }`}
+//         >
+//           <h3 className="font-semibold">Basic</h3>
+//           <p className="text-lg mt-2">₹1</p>
+//         </div>
+//       </div>
+
+//       {/* NEW USER */}
+//       <div className="border rounded-lg p-6">
+//         <h3 className="text-lg font-semibold mb-2">New Users</h3>
+//         <p className="text-sm text-gray-500 mb-6">
+//           New to Sri Angalamman Matrimony
+//         </p>
+
+//         <div className="grid grid-cols-2 gap-4">
+//           {/* <div
+//             onClick={() => {
+//               setPlan("basic");
+//               setUserType("new");
+//             }}
+//             className={`p-6 border rounded-lg cursor-pointer flex flex-col items-center justify-center min-h-[90px] ${
+//               plan === "basic" ? "border-rose-500 bg-rose-50" : ""
+//             }`}
+//           >
+//             <h3 className="font-semibold">Basic</h3>
+//             <p className="text-lg mt-2">₹2</p>
+//           </div>
+
+//           <div
+//             onClick={() => setPlan("premium")}
+//             className={`p-6 border rounded-lg cursor-pointer flex flex-col items-center justify-center min-h-[90px] ${
+//               plan === "premium" ? "border-rose-500 bg-rose-50" : ""
+//             }`}
+//           >
+//             <h3 className="font-semibold">Premium</h3>
+//             <p className="text-lg mt-2">₹3</p>
+//           </div> */}
+
+//           <div
+//             onClick={() => {
+//               setPlan("basic");
+//               setUserType("new");
+//             }}
+//             className={`p-6 border rounded-lg cursor-pointer flex flex-col items-center justify-center min-h-[90px] ${
+//               plan === "basic" && userType === "new"
+//                 ? "border-rose-500 bg-rose-50"
+//                 : ""
+//             }`}
+//           >
+//             <h3 className="font-semibold">Basic</h3>
+//             <p className="text-lg mt-2">₹2</p>
+//           </div>
+
+//           <div
+//             onClick={() => {
+//               setPlan("premium");
+//               setUserType("new");
+//             }}
+//             className={`p-6 border rounded-lg cursor-pointer flex flex-col items-center justify-center min-h-[90px] ${
+//               plan === "premium" ? "border-rose-500 bg-rose-50" : ""
+//             }`}
+//           >
+//             <h3 className="font-semibold">Premium</h3>
+//             <p className="text-lg mt-2">₹3</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+
+//     <button
+//       onClick={handlePayment}
+//       disabled={loading || !plan}
+//       className="mt-8 px-6 py-2 bg-gradient-to-r from-pink-600 to-yellow-500 text-white rounded"
+//     >
+//       {loading ? "Redirecting..." : "Pay Now"}
+//     </button>
+//   </div>
+// );
+
 return (
   <div className="max-w-4xl mx-auto mt-10 bg-white p-8 rounded-xl shadow text-center">
     <h2 className="text-xl font-bold mb-8">Choose Your Plan</h2>
@@ -168,9 +269,14 @@ return (
         </p>
 
         <div
-          onClick={() => setPlan("basic")}
+          onClick={() => {
+            setPlan("basic");
+            setUserType("old");
+          }}
           className={`p-6 border rounded-lg cursor-pointer ${
-            plan === "old_basic" ? "border-rose-500 bg-rose-50" : ""
+            plan === "basic" && userType === "old"
+              ? "border-rose-500 bg-rose-50"
+              : ""
           }`}
         >
           <h3 className="font-semibold">Basic</h3>
@@ -186,18 +292,28 @@ return (
         </p>
 
         <div className="grid grid-cols-2 gap-4">
+          {/* NEW BASIC */}
           <div
-            onClick={() => setPlan("basic")}
+            onClick={() => {
+              setPlan("basic");
+              setUserType("new");
+            }}
             className={`p-6 border rounded-lg cursor-pointer flex flex-col items-center justify-center min-h-[90px] ${
-              plan === "basic" ? "border-rose-500 bg-rose-50" : ""
+              plan === "basic" && userType === "new"
+                ? "border-rose-500 bg-rose-50"
+                : ""
             }`}
           >
             <h3 className="font-semibold">Basic</h3>
             <p className="text-lg mt-2">₹2</p>
           </div>
 
+          {/* PREMIUM */}
           <div
-            onClick={() => setPlan("premium")}
+            onClick={() => {
+              setPlan("premium");
+              setUserType("new");
+            }}
             className={`p-6 border rounded-lg cursor-pointer flex flex-col items-center justify-center min-h-[90px] ${
               plan === "premium" ? "border-rose-500 bg-rose-50" : ""
             }`}
